b53583168e50cd207f5886a71cce5b8477b152ee
Khaidz <tuankhaidx2003@gmail.com>
2024-09-01 18:48:58 +0700

    board-bar

--- src/components/AppBar/Menus/Profiles.jsx
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -31,7 +31,7 @@ function Profiles() {
           aria-haspopup="true"
           aria-expanded={open ? 'true' : undefined}
         >
-          <Avatar sx={{ width: 30, height: 30 }} alt="avatar" src="" />
+          <Avatar sx={{ width: 34, height: 34 }} alt="avatar" src="" />
         </IconButton>
       </Tooltip>
       <Menu
