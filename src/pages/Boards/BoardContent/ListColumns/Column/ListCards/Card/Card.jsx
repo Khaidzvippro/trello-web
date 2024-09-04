aaab57678fe7d82f5847d3fc008fd8bd0547004f
Khaidz <tuankhaidx2003@gmail.com>
2024-09-04 18:17:10 +0700

    update_structure

---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -0,0 +1,58 @@
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import { Card as MuiCard } from '@mui/material'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import GroupIcon from '@mui/icons-material/Group'
+import CommentIcon from '@mui/icons-material/Comment'
+import AttachmentIcon from '@mui/icons-material/Attachment'
+
+function Card({ temporaryHideMedia }) {
+  if (temporaryHideMedia) {
+    return (
+      <MuiCard
+        sx={{
+          cursor: 'pointer',
+          boxShadow: '0 1px 1px rgba(0,0,0,0.2)',
+          overflow: 'unset'
+        }}
+      >
+        <CardContent sx={{ p: 1, '&:last-child': { p: 1 } }}>
+          <Typography>Card test 01</Typography>
+        </CardContent>
+      </MuiCard>
+    )
+  }
+  return (
+    <MuiCard
+      sx={{
+        cursor: 'pointer',
+        boxShadow: '0 1px 1px rgba(0,0,0,0.2)',
+        overflow: 'unset'
+      }}
+    >
+      <CardMedia
+        sx={{ height: 140 }}
+        image="https://st.quantrimang.com/photos/image/2024/01/24/tao-hinh-nen-may-tinh-bang-ai-12.jpg"
+        title="green iguana"
+      />
+      <CardContent sx={{ p: 1, '&:last-child': { p: 1 } }}>
+        <Typography>Khaidz</Typography>
+      </CardContent>
+      <CardActions sx={{ p: '0 4px 8px 4px' }}>
+        <Button size="small" startIcon={<GroupIcon />}>
+          20
+        </Button>
+        <Button size="small" startIcon={<CommentIcon />}>
+          15
+        </Button>
+        <Button size="small" startIcon={<AttachmentIcon />}>
+          10
+        </Button>
+      </CardActions>
+    </MuiCard>
+  )
+}
+
+export default Card
